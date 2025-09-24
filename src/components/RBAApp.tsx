ec053bf699f9c0741ba10c51b68d7b7ae632a3b5
Ben Hinton <benhinton@Bens-MacBook-Pro.local>
2025-09-24 17:17:19 +1000

    Remove debug logging - identified issue: no classes in database

--- src/components/RBAApp.tsx
+++ src/components/RBAApp.tsx
@@ -36,12 +36,6 @@ export const RBAApp: React.FC<RBAAppProps> = ({ user }) => {
         getAllClasses()
       ]);
       
-      // Debug logging
-      console.log('User email:', user.email);
-      console.log('Admin status:', adminStatus);
-      console.log('All classes count:', allClasses.length);
-      console.log('All classes:', allClasses);
-      
       setIsAdmin(adminStatus);
       
       if (adminStatus) {
